305a250dd307d54a2ff89c286246014320ead474
Michal Turcan <michal.turcan.za@gmail.com>
2023-03-14 13:57:03 +0100

    change user from ubuntu to terra

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo chown ubuntu:ubuntu /entrypoint/*
+sudo chown terra:terra /entrypoint/*
 chmod +x /entrypoint/*.sh
 find /entrypoint/*.sh -maxdepth 1 -type f -exec {} \; | tee -a /var/log/cosmovisor/entrypoint.log
